868b2daec170f654e9ae6d8fe1c7cc457a5f1069
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-01-25 19:03:13 +0530

    Basic use of BeautifulSoup library

--- BeautifulSoupExample.py
+++ BeautifulSoupExample.py
@@ -2,6 +2,7 @@ import bs4
 import requests
 
 #bs4 contain BeautifulSoup class which is used to extract information out of html page
+
 r = requests.get('http://www.piemr.edu.in')
 s = bs4.BeautifulSoup(r.text)
 e = s.select('div')
