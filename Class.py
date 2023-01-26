fe8b65946cda1d24e824099d5d21982bb091d449
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-01-26 14:05:25 +0530

    Add files via upload

---
+++ Class.py
@@ -0,0 +1,25 @@
+class Employee:
+    emp = 0
+    def __init__(self,name,email,age):
+        self.name = name
+        self.email = email
+        self.age = age
+        Employee.emp += 1
+    def displayCount(self):
+        print("Total Number of Employee is :- " , Employee.emp)
+    def displayInfo(self):
+        print("Name :- " , self.name , " Email :- " , self.email , " Age :- " , self.age)
+
+class Student:
+    count = 0
+    def __init__(self,fname,lname,subject,grade):
+        self.fname = fname
+        self.lname = lname
+        self.subject = subject
+        self.grade = grade
+        Student.count += 1
+    def studentInfo(self):
+        print("First name :- " , self.fname)
+        print("Last name :- " , self.lname)
+        print("Subject :- " , self.subject)
+        print("Grades :- " , self.grade)
