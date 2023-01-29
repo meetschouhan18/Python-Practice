6dda2fd64ea2b8f0ac856b4e02ab6a0acbdadb99
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-01-29 10:59:11 +0530

    os read write examples

--- FileReadWrite.py
+++ FileReadWrite.py
@@ -1,30 +1,40 @@
 import os
 print(os.getcwd())
 
-p = open('a.txt','w')     #Opening an exisiting file to write only
+p = open('a.txt','w')
+#Opening an exisiting file to write only
 q = 'Meet'
-p.write(q)                     #Previously exisitng data is deleted and string q is written in its place
-#print(p.write(q))           #It prints no. of bytes we added after removing previous data(18 bytes in this case)
+p.write(q)
+#Previously exisitng data is deleted and string q is written in its place
+#print(p.write(q))
+#It prints no. of bytes we added after removing previous data(18 bytes in this case)
 p.close()
 
-p = open('a.txt','r')     #Opening an existing file to read only
+p = open('a.txt','r')
+#Opening an existing file to read only
 print(p.read())
 p.close()
 
-p = open('a.txt','a')      #Opening an exisiting file to append data
+p = open('a.txt','a')
+#Opening an exisiting file to append data
 q = ' Singh Chouhan'
-p.write(q)                      #No data is deleted, New data is added
-#print(p.write(q))            #Prints no. of bytes added
+p.write(q)
+#No data is deleted, New data is added
+
+#print(p.write(q))
+#Prints no. of bytes added
 p.close()
 
 p = open('b.txt','r')
-for line in p.readlines():     #readlines is used to read multiple lines
+for line in p.readlines():
+    #readlines is used to read multiple lines
     print(line)
 p.close()
 
 p = open('b.txt','r')
 print("Getting rid of new lines")
 for line in p.readlines():
-    line = line.strip('\n')    #we stripped every \n
+    line = line.strip('\n')
+    #we stripped every \n
     print(line)
 p.close()
