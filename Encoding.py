8553332929965f1fd2dd4e79c337d229ffcc5722
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-01-29 10:49:49 +0530

    Encoding using requests library

--- Encoding.py
+++ Encoding.py
@@ -1,10 +1,18 @@
 import requests
 r = requests.get('https://api.github.com/events')
+
 #.get function automatically decode data
-print(r.encoding)     #tells us which encoding is applied on data
+print(r.encoding)
+#tells us which encoding is applied on data
+
 r.encoding = 'ISO-8859-1'
-print(r.encoding)    #encoding changed
+print(r.encoding)
+#encoding changed
 result = requests.get('https://api.github.com/events',stream=True)
+
 #raw is uesd in rare cases when we actually want to get these socket responses
-print(result.raw)     # it created the actual http response object at this memory address
-print(result.raw.read(15))   #we are reading response object of first 15 bytes
+print(result.raw)
+# it created the actual http response object at this memory address
+
+print(result.raw.read(15))
+#we are reading response object of first 15 bytes
