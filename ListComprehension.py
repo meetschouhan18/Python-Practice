c75bb8f47cd2ccf7e8befc72b1350ef04a3406a5
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-02-01 21:51:15 +0530

    list comprehension example

--- ListComprehension.py
+++ ListComprehension.py
@@ -1,8 +1,5 @@
-Q1.) Below, we have provided a list of tuples that contain students’ names and their final grades in PYTHON 101.
-Using list comprehension, create a new list passed that contains the names of students who passed the class (had a final grade of 70 or greater).
+#Q1.) Below, we have provided a list of tuples that contain students’ names and their final grades in PYTHON 101.
+#Using list comprehension, create a new list passed that contains the names of students who passed the class (had a final grade of 70 or greater).
 
 students = [('Tommy', 95), ('Linda', 63), ('Carl', 70), ('Bob', 100), ('Raymond', 50), ('Sue', 75)]
 passed = [student[0] for student in students if student[1]>=70]
-
-
-
