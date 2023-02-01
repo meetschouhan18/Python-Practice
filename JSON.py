f2fd65e1c19e2150baa60bde508bf1056586fe6d
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-02-01 21:41:27 +0530

    Add files via upload

---
+++ JSON.py
@@ -0,0 +1,19 @@
+import json
+data = '''{
+    "name" : "Meet" ,
+    "phone" : {
+        "type" : "int1" ,
+        "number" : "+1 734 303 4456"
+    } ,
+    "email" : {
+        "hide" : "yes"
+    }
+}'''
+info = json.loads(data) # converts data from JSON format to dictionary
+print("Name :- " , info["name"])
+print("Hide :- " , info["email"]["hide"])
+
+def p(obj):
+  return json.dumps(obj , sort_keys=True, indent=2)
+#dictionary is printed in json format, in alphabetical order(since we used sort_keys) and well indented(to help us understand better)
+print(p({'a' : 5 , 'b' : 6}))
---
+++ Lambda.py
@@ -0,0 +1,7 @@
+# lambda is used to create one line function , which means short form of functions , used to reduce no. of line
+# def func(args):
+#    return ret_val
+# can be written as
+# func = lambda args: ret_val
+m = lambda a, b, c : a + b
+print(m(1,2,3))
