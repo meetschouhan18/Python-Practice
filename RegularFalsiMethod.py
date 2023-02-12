d89ecfe0636a83af32e7eb38f38a9210188962e0
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-02-12 22:25:20 +0530

    Roots of equation using Regular Falsi

--- RegularFalsiMethod.py
+++ RegularFalsiMethod.py
@@ -1,25 +1,33 @@
 t = 0
 print("Equation is of the form a*x^3 + b*x^2 + c*x + d ");
+
 a = int(input("Enter the value of a :- "))
 b = int(input("Enter the value of b :- "))
 c = int(input("Enter the value of c :- "))
 d = int(input("Enter the value of d :- "))
+
 while t == 0:
 	x = int(input("Enter the value of x at which f(x) is to be calculated :- "))
 	e = a*x*x*x + b*x*x + c*x + d
 	print("Value of f(x) at x = ", x ," is   ",e)
+
 	t = int(input("Enter 0 to continue calculating f(x) :- "))
+
 print("After calculating different values of f(x) you must have got a negative and a positive value of f(x) at x being two consecutive integers...")
+
 m = int(input("Enter value of x at which f(x) is positive :- "));
 n = int(input("Enter value of x at which f(x) is negative :- "));
+
 for i in range (0, 12):
 	p = a*m*m*m + b*m*m + c*m +d
 	q = a*n*n*n + b*n*n + c*n +d
 	o = n - ((m - n) / (p - q)) * q
 	f = a*o*o*o + b*o*o + c*o + d
+
 	print("Value of f(x) at x = ",o," is   ",f ,"\n")
 	if f > 0:
 		m = o
 	if f < 0:
 		n = o
+
 print("Thus Root of the equation(upto 5 decimal places) is :- ",round(o, 5),"      (Using Regular Falsi Method)")
