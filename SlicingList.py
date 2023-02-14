aa9df1ac02318649296a871ce9f801ca52300f70
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-02-14 15:37:13 +0530

    Slicing in lists

--- SlicingList.py
+++ SlicingList.py
@@ -1,10 +1,14 @@
 from statistics import mean
+
 li = [90,44,75,66,54,26,56,88,56,47,66,45,35,69,75,68,98]
 li.sort()
+
 x = int(0.1*len(li))
 li = li[x:len(li)-x]
+
 print(li)
 print(mean(li))
+
 
 #list is sorted and using slicing list's 1st and last element is discarded and mean is calculated from the remaining list
 #Example of slicing list
