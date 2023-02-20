13ee5e65604c07dbd230aca7406ecb1a7c19e0de
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-02-20 17:09:39 +0530

    socket example

--- Socket.py
+++ Socket.py
@@ -1,14 +1,19 @@
 import socket
+
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect( ('data.pr4e.org' , 80) )
 cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
 #encode converts unicode(string) in UTF-8 code
+
 mysock.send(cmd)
+
 while True:
     data = mysock.recv(512)
-#data variable recieves 512 characters
+    #data variable recieves 512 characters
+    
     if (len(data) < 1):
         break
     print(data.decode())
+
 #converts UTF-8 encoded data into unicode
 mysock.close()
