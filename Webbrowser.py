a1d5e9dec8f6da7e799f2694091ba7dd770b873b
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-02-23 16:10:37 +0530

    Add files via upload

---
+++ Webbrowser.py
@@ -0,0 +1,3 @@
+import webbrowser
+# webbrowser allow us to browse
+webbrowser.open('https://facebook.com')   #opens specified url in opera or internet explorer
---
+++ Zip.py
@@ -0,0 +1,14 @@
+#zip function
+
+l1 = [1,2,3]
+l2 = [4,5,6]
+l3 = list(zip(l1, l2))
+print(l3)
+#this returns l3 = [(1,4), (2,5), (3,6)]
+# it creates a tuple of elements present at the same position in the lists given in argument
+l4 = []
+for (x1, x2) in l3:
+  l4.append(x1+x2)
+print(l4)
+
+# this outputs the list of sum of the tuples created by zip file
