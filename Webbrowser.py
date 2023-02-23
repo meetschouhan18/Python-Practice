6a5701f41c4c187605494da3b578792c43cfb135
Meet Singh Chouhan <64706251+meetschouhan18@users.noreply.github.com>
2023-02-23 16:11:41 +0530

    webbrowser library example

--- Webbrowser.py
+++ Webbrowser.py
@@ -1,3 +1,5 @@
 import webbrowser
 # webbrowser allow us to browse
-webbrowser.open('https://facebook.com')   #opens specified url in opera or internet explorer
+
+webbrowser.open('https://facebook.com')
+#opens specified url in opera or internet explorer
